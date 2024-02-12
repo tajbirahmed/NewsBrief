8d0702e2c949fbc8711401983ff8f40e431984ce
tajbirahmed <tajbir26@gmail.com>
2024-02-12 21:29:21 +0600

    current working

--- app/NewsStand.tsx
+++ app/NewsStand.tsx
@@ -8,32 +8,41 @@ import { ScrollView, View, Text, StyleSheet, useColorScheme } from 'react-native
 // 1. Header title is not yet configured
 // verdict: not completed
 
-
-// 1. Header title is not yet configured
-// verdict: not completed
-
 const NewsStand = () => {
     const colorScheme = useColorScheme();
     const colorVal = colorScheme === 'dark' ? 'white' : 'black';
     const bgVal = colorScheme === 'dark' ? 'black' : 'white';
     return (
-        <ScrollView contentContainerStyle={ styles.container }>
-            {categories.map((value, index) => (
-                <NewsStandSlider
-                    key={index}
-                    categoryName={value.categoryName}
-                    child={value.child}
-                />
-            ))}
-        </ScrollView>
-    )
+        <View style={[styles.container, {backgroundColor: bgVal}]}> 
+            <View>
+                <Text style={[styles.subheading_text, { color: colorVal, }]}>
+                    Suggested Sources
+                </Text>
+            </View>
+            <ScrollView contentContainerStyle={ styles.container }>
+                {categories.map((value, index) => (
+                    <NewsStandSlider
+                        key={index}
+                        categoryName={value.categoryName}
+                        child={value.child}
+                    />
+                ))}
+            </ScrollView>
+        </View>
+            )
 }
 
-export default NewsStand;
+export default NewsStand; 
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, 
         overflow: 'scroll', 
+    }, 
+    subheading_text: {
+        fontSize: 10,
+        textAlign: 'center',
+        fontWeight: '500',
+        paddingTop: 3,
+        paddingBottom: 2,
     }
 })
