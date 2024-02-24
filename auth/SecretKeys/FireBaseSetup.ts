81dc25183601d1be71208149004fe447f22a32a2
tajbirahmed <tajbir26@gmail.com>
2024-02-24 11:25:46 +0600

    current working

---
+++ auth/SecretKeys/FireBaseSetup.ts
@@ -0,0 +1,7 @@
+export const apiKey = "AIzaSyB8ZHOJNvextWbXEsCLAw91Hn8ecQUrx-g"; 
+export const authDomain = "newsbiref.firebaseapp.com"; 
+export const projectId = "newsbiref"; 
+export const storageBucket = "newsbiref.appspot.com"; 
+export const messagingSenderId = "262325174661"; 
+export const appId = "1:262325174661:web:b880bf6fb04b18b5fec4ec"; 
+export const measurementId = "G-0DDVP90XVS";
